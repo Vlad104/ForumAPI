5c72c0589b65c819b1b3ffbf8506613ab2e67747
Vlad104 <vlad104khk@gmail.com>
2019-05-31 16:12:23 +0300

    delete some

--- database/sql/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-psql --command "CREATE USER docker WITH SUPERUSER PASSWORD 'docker';"
-createdb -O docker docker
-psql -d docker -c "CREATE EXTENSION IF NOT EXISTS citext;"
-psql docker -f ./init.sql
